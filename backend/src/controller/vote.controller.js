d6c2c8396cd9455c0178e8f8d9c181ce7020a9e7
mvazquez <matias.vazquezmartin@gmail.com>
2024-01-18 22:01:50 -0300

    fix: vote.controller

--- backend/src/controller/vote.controller.js
+++ backend/src/controller/vote.controller.js
@@ -28,7 +28,6 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const data = req.body;
-
     // res.status(200).json(data);
     const response = await VoteService.create();
 
